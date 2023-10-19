c704b9d52f3d1bf4120f9b57caebb118f3191469
Yannick <girofare@hotmail.com>
2023-10-19 22:52:16 +0200

    added next themes and lucide react and themeprovider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { Alert } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Textarea } from '@/components/ui/textarea'
+import { Avatar } from '@radix-ui/react-avatar'
 import Image from 'next/image'
+import { Input } from '@/components/ui/input'
+import { Form } from 'react-hook-form'
 
 export default function Home() {
   return (
-    <div>hello world !</div>
+    <div className='m-4'>
+    <Button>click me !</Button>
+    <Input/>
+    </div>
   )
 }
