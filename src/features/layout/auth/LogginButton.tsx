8aceb6682f197ab48e1f69cec594d3cde021e425
Yannick <girofare@hotmail.com>
2023-10-23 21:41:47 +0200

    added usetransition and loading in loginbutton

--- src/features/layout/auth/LogginButton.tsx
+++ src/features/layout/auth/LogginButton.tsx
@@ -2,16 +2,25 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { LogIn } from 'lucide-react'
-import React from 'react'
+import { Loader, LogIn } from 'lucide-react'
+import { useTransition } from 'react'
 import {signIn} from 'next-auth/react'
 
 export const LogginButton = () => {
-  return (
-    <Button onClick={()=>{
-        signIn();
+  const [isPending, startTransition] = useTransition();
+  
+  return (  
+    <Button 
+        onClick={()=>{
+        startTransition(() => signIn());
     }}>
-        <LogIn className='mr-2 h-4 m-4'/>
+        {isPending ? (
+            <Loader className = "mr-2 h-4 w-4"/>
+            ):(
+                <LogIn className = "mr-2 h-4 w-4"/>
+            
+        )}
+        {/* {isPending ? "loading" : "Login"} */}
         Login
     </Button>
   )
