bacbac8a73b32cce90e9dd2a8a3db82f4fa4de5d
Yannick <girofare@hotmail.com>
2023-11-24 23:58:33 +0100

    added profil page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,45 @@
+import { Profile } from '@/app/users/[userId]/Profile';
+import { followUser } from '@/app/users/[userId]/follow.action';
+import { Button } from '@/components/ui/button';
+import { getAuthSession } from '@/lib/auth';
+import { prisma } from '@/lib/prisma';
+import { getUserProfile } from '@/src/query/user.query';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
-export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
+export default async function ProfilePage({params}:{params:{userId:string}}) {
+    const session = await getAuthSession();
+    const user = await getUserProfile(params.userId )
+    if (!user) {
+        notFound()
+      }
+      const isFollowing = session?.user.id ? await prisma.follow.findFirst({
+        where: {
+            followerId: user.id,
+            followingId: user.id
+        },
+    }) : null
+    const isCurrentUser = params.userId === session?.user.id;
+        let FollowButton
+    if (!isCurrentUser) {
+        FollowButton = (<form className='mt-4'>
+        <Button variant={'outline'} formAction={async () => {
+            'use server';
+            if(!session?.user.id ) {
+                return;
+            }
+            await followUser(params.userId)}}>
+            {isFollowing ? "Unfollow" : "Follow"}
+        </Button>
+    </form>)}
+        return (
+             
+        <div className='divide-y divide-accent border-t border-accent'>
+            <Profile user={user}/>
+          
+                <form className='mt-4'>
+                    {FollowButton}
+                </form>
+        </div>
+      )
 }
