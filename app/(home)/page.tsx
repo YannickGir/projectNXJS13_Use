2967cf2040fc5e1aea9db95d49f0371f60114de4
Yannick <girofare@hotmail.com>
2023-11-28 10:59:36 +0100

    added loader

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -10,6 +10,8 @@ import { getLatestPosts } from '@/src/query/post.query';
 export default async function Home() {
     const session = await getAuthSession();
     const posts = await getLatestPosts(session?.user.id);
+
+    await new Promise ((r) => setTimeout(r, 3000))
   return (
     <div className='divide-y divide-muted'>
         {/* <p>{JSON.stringify(session, null, 2)}</p> */}
